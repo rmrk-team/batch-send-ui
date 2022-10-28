f49975463339f5aef47b523bd9d96ab81a891cbd
Yuri <yuripetusko@gmail.com>
2022-10-28 12:27:14 +0100

    fix wallet sync

--- lib/web3-from-source.ts
+++ lib/web3-from-source.ts
@@ -2,7 +2,7 @@ import { useWalletsStore, getWalletBySource, WALLET_EXTENSIONS } from '@rmrk-tea
 
 export const web3FromSource = () => {
   const { selectedWallet } = useWalletsStore.getState();
-  const wallet = getWalletBySource(selectedWallet as WALLET_EXTENSIONS);
 
+  const wallet = getWalletBySource(selectedWallet as WALLET_EXTENSIONS);
   return wallet?.extension;
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,23 +3,25 @@ import dynamic from 'next/dynamic';
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import React from 'react';
+import { AccountStoreSync } from '@rmrk-team/dotsama-wallet-react';
 const SubstraHooksProviderSSR = dynamic(() => import('../components/app/substra-hooks-provider'), {
   ssr: false,
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-      <SubstraHooksProviderSSR
-          apiProviderConfig={{
-            kusama: {
-              id: 'kusama',
-              wsProviderUrl: 'wss://kusama-rpc.polkadot.io',
-            },
-          }}>
-        <ChakraProvider resetCSS>
-          <Component {...pageProps} />
-        </ChakraProvider>
-      </SubstraHooksProviderSSR>
+    <SubstraHooksProviderSSR
+      apiProviderConfig={{
+        kusama: {
+          id: 'kusama',
+          wsProviderUrl: 'wss://kusama-rpc.polkadot.io',
+        },
+      }}>
+      <ChakraProvider resetCSS>
+        <AccountStoreSync />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </SubstraHooksProviderSSR>
   );
 }
 
